f60b2123a1dbdcd00a8a1bbd2aab973bcca17220
basildane <basildane@users.sourceforge.net>
2014-06-09 09:10:28 -0400

    Release 2.10.10

--- Console/WakeOnLanC/My Project/AssemblyInfo.vb
+++ Console/WakeOnLanC/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.10.9.3")> 
-<Assembly: AssemblyFileVersion("2.10.9.3")> 
+<Assembly: AssemblyVersion("2.10.10.0")> 
+<Assembly: AssemblyFileVersion("2.10.10.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
