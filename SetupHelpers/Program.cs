5ea8c2a29056cb1fda02c6f95ce09bba34284b3e
basildane <basildane@users.sourceforge.net>
2015-03-17 13:36:50 -0400

    Remove Installshield.  Add Inno Setup.

---
+++ SetupHelpers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SetupHelpers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String eventSource = "WakeOnLAN";
+
+            switch (args[0])
+            {
+                case "/install":
+                    if (!EventLog.SourceExists(eventSource))
+                        EventLog.CreateEventSource(eventSource, "Application");
+                    break;
+
+                case "/uninstall":
+                    if (EventLog.SourceExists(eventSource))
+                        EventLog.DeleteEventSource(eventSource);
+                    break;
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
