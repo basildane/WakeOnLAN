c98f54bb5fff97cb3592a1daefd3d8f1332bbe7f
basildane <basildane@users.sourceforge.net>
2015-03-17 20:09:35 -0400

    Added SetupHelpers function that is called by the installer to finalize configuration.

--- SetupHelpers/Program.cs
+++ SetupHelpers/Program.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿/*
+ *   WakeOnLAN - Wake On LAN
+ *    Copyright (C) 2004-2015 Aquila Technology, LLC. <webmaster@aquilatech.com>
+ *
+ *    This file is part of WakeOnLAN.
+ *
+ *    WakeOnLAN is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    WakeOnLAN is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with WakeOnLAN.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Diagnostics;
-using System.Threading;
 
 namespace SetupHelpers
 {
     class Program
     {
+        /// <summary>
+        /// This program is called by the installer to perform final setup before launching WOL.
+        /// Creates the eventlog Event Source.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             const String eventSource = "WakeOnLAN";
@@ -22,7 +46,6 @@ namespace SetupHelpers
                         EventLog.DeleteEventSource(eventSource);
                     break;
             }
-            Thread.Sleep(1000);
         }
     }
 }
