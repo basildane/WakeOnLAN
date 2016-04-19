8c9dbbbb53ece2378bd5f520b47ff3321e8b0313
basildane <webmaster@aquilatech.com>
2016-04-19 12:18:44 -0400

    Beta 2.11.6.20.
    AutoUpdater.NET, History, EventLogViewer.

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -49,7 +49,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.11.6.10")>
+<Assembly: AssemblyVersion("2.11.6.20")>
 
 <Assembly: ComVisibleAttribute(False)> 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
