36db69afe3371e86fbe42144a4f4e6a13bf1ffe5
basildane <webmaster@aquilatech.com>
2016-05-18 10:30:10 -0400

    Retarget for DotNet 4.0.  This is to support XP and 2003 server.

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -49,7 +49,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.11.9.10")>
+<Assembly: AssemblyVersion("2.11.9.15")>
 
 <Assembly: ComVisibleAttribute(False)> 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
