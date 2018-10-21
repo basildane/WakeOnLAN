2a429af73b933d6edad28a3895df97ca17bb5271
basildane <basildane@aquilatech.com>
2018-10-21 14:14:26 -0400

    Release 2.12.0.

--- AlphaWindow/AssemblyInfo.cs
+++ AlphaWindow/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.10.15.0")]
+[assembly: AssemblyVersion("2.10.16.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
