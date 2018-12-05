bc233ac856b8784b74ac33adecd2954ab5218191
basildane <basildane@aquilatech.com>
2018-12-05 17:55:09 -0500

    Release 2.12.2

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -49,7 +49,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.12.1.10")>
+<Assembly: AssemblyVersion("2.12.2.0")>
 
 <Assembly: ComVisibleAttribute(False)> 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
